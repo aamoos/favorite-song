95ae89cf50cea1e1a30409967f2e26e4be41ba8c
aamoos <aamoos@naver.com>
2024-04-12 00:43:29 +0900

    header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,8 +4,9 @@
       <h1 class="text-white text-1xl font-bold">Favorite Song</h1>
       <nav>
         <ul class="flex space-x-4">
-          <li><a href="#" class="text-white">노래찾기</a></li>
+          <li><a href="/search" class="text-white">노래찾기</a></li>
           <li><a href="#" class="text-white">즐겨찾기</a></li>
+          <li><a href="#" class="text-white">로그아웃</a></li>
         </ul>
       </nav>
     </div>
