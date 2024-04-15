159588c3b54782cadbf906fdba634bf605613164
김재성 <aamoos@openmate.co.kr>
2024-04-15 18:16:53 +0900

    전역변수 설정, 로그인페이지 변경

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+   devServer: {
+    port : 8080,
+    proxy : process.env.VUE_APP_BACKEND_URL
+  },
   transpileDependencies: true
 })
