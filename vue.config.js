8423c71c6ac5608b9494ce10e17317e4fabcc4f8
김재성 <aamoos@openmate.co.kr>
2024-05-27 17:52:01 +0900

    수정

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-   devServer: {
-    port : 8080,
-    proxy : process.env.VUE_APP_BACKEND_URL
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: process.env.VUE_APP_BACKEND_URL,
+        changeOrigin: true,
+        secure: false // Disable certificate verification for self-signed certificates
+      }
+    }
   },
   transpileDependencies: true
-})
+});
