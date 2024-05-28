1595f8c3afe8887aa5207c8a3d3390f548d6c220
kimjaesung <aamoos@naver.com>
2024-05-28 18:01:02 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
    devServer: {
-    port : 8080,
+    //port : 8080,
+    port : 443,
     proxy : process.env.VUE_APP_BACKEND_URL
   },
   transpileDependencies: true
