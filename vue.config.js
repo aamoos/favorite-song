207806d8065b15bb341de55b5385534a6d1ba2e7
kimjaesung <aamoos@naver.com>
2024-05-28 17:00:15 +0900

    수정

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
    devServer: {
     port : 8080,
-    // proxy : process.env.VUE_APP_BACKEND_URL
-    proxy: {
-      '/api': { 
-        target: process.env.VUE_APP_BACKEND_URL,
-        changeOrigin: true,
-      },
-      '/auth': {
-        target: process.env.VUE_APP_BACKEND_URL,
-        changeOrigin: true,
-      }
-    }
+    proxy : process.env.VUE_APP_BACKEND_URL
   },
   transpileDependencies: true
 })
