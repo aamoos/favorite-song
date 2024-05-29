44068a40c6d72456cb005d96244040ab7d4b5aa0
김재성 <aamoos@openmate.co.kr>
2024-05-29 13:56:59 +0900

    backend 8080 frontend 3000

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
    devServer: {
-    //port : 8080,
-    port : 80,
+    port : 3000,
     proxy : process.env.VUE_APP_BACKEND_URL
   },
   transpileDependencies: true
