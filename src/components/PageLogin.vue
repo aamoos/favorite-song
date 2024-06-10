65bddae1a317c9c731db2a66fc99af8ecb32b053
김재성 <aamoos@openmate.co.kr>
2024-06-10 14:54:53 +0900

    test

--- src/components/PageLogin.vue
+++ src/components/PageLogin.vue
@@ -103,8 +103,7 @@ const login = async () => {
 
 //소셜로그인
 const socialLogin = (target) => {
-  console.log(process.env.VUE_APP_BACKEND_URL + '/oauth2/authorize/' + target + '?redirect_uri=' + process.env.VUE_APP_FRONTEND_URL + '/oauth2/redirect');
-  //window.location.href = process.env.VUE_APP_BACKEND_URL + '/oauth2/authorize/' + target + '?redirect_uri=' + process.env.VUE_APP_FRONTEND_URL + '/oauth2/redirect';
+  window.location.href = process.env.VUE_APP_BACKEND_URL + '/oauth2/authorize/' + target + '?redirect_uri=' + process.env.VUE_APP_FRONTEND_URL + '/oauth2/redirect';
 };
 
 
